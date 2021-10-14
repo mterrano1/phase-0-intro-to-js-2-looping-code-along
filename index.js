08da9937f6e34dbc43286d8b1b8cb2d35760f330
Christopher Terrano <mterrano1@yahoo.com>
2021-10-13 19:57:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(people, event, message) {
+    message = [];
+      for (let i = 0; i < people.length; i++) {
+        message.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+      }
+  
+      return message;
+  }
+  
+  writeCards(["Justin", "Max", "Corey"], "birthday")
+
+
+  function countDown(posInt) {
+    let i = 0;
+    while (i <= posInt) {
+      console.log(posInt - i);
+      i++;
+    }
+  }
+  
+  countDown(13);
