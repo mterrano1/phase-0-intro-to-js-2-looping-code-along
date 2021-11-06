c7abd2e84f79a0f9eced3422ee27bb59191a6b90
Christopher Terrano <mterrano1@yahoo.com>
2021-11-06 12:07:45 -0500

    commit tree d359056e9784cee368d3c318844e0e954270b9da

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-// Code your solutions in this file
-function writeCards(people, event, message) {
-    message = [];
-      for (let i = 0; i < people.length; i++) {
-        message.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
-      }
-  
-      return message;
-  }
-  
-  writeCards(["Justin", "Max", "Corey"], "birthday")
+const names = ["Fred", "Bob", "Bill", "William"]
 
-
-  function countDown(posInt) {
-    let i = 0;
-    while (i <= posInt) {
-      console.log(posInt - i);
-      i++;
-    }
+function writeCards(name, event) {
+    const message = [];
+  for (let i = 0; i < name.length; i++) {
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
   }
-  
-  countDown(13);
+  return message; 
+}
+
+function countDown(int) {
+  let counter = int;
+  while (counter >= 0) {
+    console.log(counter--);
+  }
+}
+
